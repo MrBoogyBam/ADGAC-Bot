24c8089194df5b2726ad8f0c8b0466145e830ed9
MrBoogyBam <MrBoogyBamBusiness@gmail.com>
2024-05-26 00:06:16 +0300

    Bot will git pull on startup

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,19 @@
 const { Events } = require("discord.js");
+const path = require("node:path");
+
+const simpleGit = require("simple-git");
+const git = simpleGit(path.resolve(__dirname + "./../"));
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    execute(client) {
+    async execute(client) {
+        try {
+            await git.pull();
+        } catch(error) {
+            console.error("Failed to git pull:", error)
+        }
+
         console.log(`Ready! Logged in as ${client.user.tag}`);
     }
 };
